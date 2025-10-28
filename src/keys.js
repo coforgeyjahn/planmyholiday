39438c7fe1000514d5f825f21e478250354d109c
Charlotte Forgey-Jahn <coforgeyjahn@gmail.com>
2025-10-28 21:07:47 +0000

    Delete src/keys.js

--- src/keys.js
+++
@@ -1,6 +0,0 @@
-export const REACT_APP_UNSPLASH_ACCESS_KEY="zMnpU9j6HVxa-iWT4hlPrVuPhamAMiyzX5JWnJvS9lI";
-export const REACT_APP_GOOGLE_API_KEY="AIzaSyAWiZ2bLXmhSO8uUV9bZgKD2HWk7FXn2AQ";
-export const REACT_APP_HOSTELWORLD_API_KEY="DrHDO6gV"; // api key 
-export const REACT_APP_HOSTELWORLD_ACCESS_KEY="JCdzW4nM1e"; // User application key
-export const HOSTELWORLD_USER_ID='1011l396902' // user id
-export const HOSTELWORLD_PERSONAL_TOKEN='SkNkelc0bk0xZTpEckhETzZnVg'
